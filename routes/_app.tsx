8b028cdf4e7535b9cfc4121b1b210698dfe85727
custompro98 <mitchjoa@gmail.com>
2023-08-11 15:39:27 -0400

    extract header, tailwind it

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,25 +1,14 @@
 import { AppProps } from "$fresh/server.ts";
-import { Head, asset } from "$fresh/runtime.ts";
+import { Head } from "$fresh/runtime.ts";
+import Header from "../components/header.tsx";
 
 export default function App({ Component }: AppProps) {
   return (
     <>
       <Head>
-        <title>deno-todo</title>
+        <title>FreshDo</title>
       </Head>
-      <header className="flex flex-row space-x-4 max-w-7xl min-w-md mx-auto px-4">
-        <nav >
-          <a href="/">
-            <img src="/logo.svg" alt="Dripping lemon" className="inline" />
-          </a>
-          <a
-            href="/todos"
-            className="text-blue-500 hover:text-blue-600 text-underline"
-          >
-            Todos
-          </a>
-        </nav>
-      </header>
+      <Header />
       <main className="max-w-7xl min-w-md mx-auto px-4">
         <Component />
       </main>
