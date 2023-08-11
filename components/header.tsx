8d24388e8157ba5f52c59bc0e09ef743dc0635da
custompro98 <mitchjoa@gmail.com>
2023-08-11 16:17:56 -0400

    stop header resizing

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
           <a href="/" className="flex items-center">
             <img
               src="/logo.svg"
-              className="h-6 sm:h-9"
+              className="h-9"
               alt="Flowbite Logo"
             />
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
@@ -16,7 +16,7 @@ export default function Header() {
           <div className="flex items-center">
             <a
               href="#"
-              className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+              className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
             >
               Log in
             </a>
