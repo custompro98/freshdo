dadb78c0d8cf1f2cb22b8bd7b60a697b761a32c2
custompro98 <mitchjoa@gmail.com>
2023-08-14 13:27:24 -0400

    support libsql database connections (i.e. Turso)

--- database/index.ts
+++ database/index.ts
@@ -1,14 +1,22 @@
 import { Kysely } from "kysely";
 import { DenoSqliteDialect } from "kysely-deno-sqlite";
 import { DB as Sqlite } from "sqlite";
+import { LibsqlDialect } from "kysely-turso";
 
 import { type Database } from "./types.ts";
 import environment from "../utility/environment.ts";
 
 const DATABASE_URL = environment.get("DATABASE_URL");
 
+const dialect = DATABASE_URL.startsWith("libsql://")
+  ? new LibsqlDialect({
+      url: DATABASE_URL,
+      authToken: environment.get("DATABASE_AUTH_TOKEN"),
+    })
+  : new DenoSqliteDialect({
+      database: new Sqlite(DATABASE_URL),
+    });
+
 export default new Kysely<Database>({
-  dialect: new DenoSqliteDialect({
-    database: new Sqlite(DATABASE_URL),
-  }),
+  dialect,
 });
